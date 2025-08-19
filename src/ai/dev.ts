90f1e5f34da18b2c052625db9856cfde4afb771d
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-08-19 15:17:07 +0000

    error terus

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,5 +2,5 @@ import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/grammar-correction';
-import '@/ai/flows/ai-conversation-grammar-suggestions';
 import '@/ai/flows/ai-conversation';
+import '@/ai/flows/ai-conversation-grammar-suggestions';
