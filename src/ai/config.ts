995b4ec04629c6cbf890534a9fd0ee5ce47f4d1a
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-08-19 16:57:36 +0000

    1/1
    Next.js 15.3.3 (stale)
    Turbopack
    Runtime Error
    
    TypeError: {import

--- src/ai/config.ts
+++ src/ai/config.ts
@@ -3,16 +3,16 @@
  */
 import {googleAI} from '@genkit-ai/googleai';
 import {genkit} from 'genkit';
-import * as firebase from '@genkit-ai/firebase';
-import * as next from '@genkit-ai/next';
+import firebase from '@genkit-ai/firebase';
+import next from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
     }),
-    (firebase as any).firebase(),
-    (next as any).next(),
+    firebase(),
+    next(),
   ],
   flowStateStore: 'firebase',
   traceStore: 'firebase',
