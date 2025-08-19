a57aff984834f7a247e40f7bfa9d81fe2170fb68
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-08-19 16:59:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/config.ts
+++ src/ai/config.ts
@@ -3,17 +3,11 @@
  */
 import {googleAI} from '@genkit-ai/googleai';
 import {genkit} from 'genkit';
-import * as firebase from '@genkit-ai/firebase';
-import * as next from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
     }),
-    firebase.firebase(),
-    next.next(),
   ],
-  flowStateStore: 'firebase',
-  traceStore: 'firebase',
 });
